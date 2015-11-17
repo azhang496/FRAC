8c66cb084b71062e911ff1e53e45fc0ce19fcf4e
Annie Zhang <azhang496@gmail.com>
2015-11-17 00:35:59 -0500

    Non-main function declarations working

--- compile.ml
+++ compile.ml
@@ -14,8 +14,7 @@ let rec expr = function
     Literal i -> string_of_int i
   | Id s -> s
   | String s -> "\"" ^ s ^ "\""
-  | Binop (e1, o, e2) -> (expr e1) ^ (
-      match o with
+  | Binop (e1, o, e2) -> (expr e1) ^ (match o with
         Add -> " + " | Sub -> " - " | Mult -> " * " | Div -> " / "
       | Equal -> " == " | Neq -> " != "
       | Less -> " < " | Leq -> " <= " | Greater -> " > " | Geq -> " >= "
@@ -35,17 +34,19 @@ let rec stmt = function
       "else" ^ "{\n" ^ (stmt st2) ^ "}\n"
   | While (e, st) -> "while(" ^ (expr e) ^ ") {\n" ^ (stmt st) ^ "}\n"
 
+let add_type v = "type " ^ v
+
 let gen_fdecl fdecl =
   (match fdecl.fname with
       "main" -> "int main()"
-    | _      -> "func " ^ fdecl.fname) ^ (* THIS IS WRONG *)
+    | _      -> "return_type " ^ fdecl.fname ^ "(" ^ (String.concat ", " (List.map add_type fdecl.formals)) ^ ")") ^
 "{\n" ^ String.concat "" (List.map stmt fdecl.body) ^
   (match fdecl.fname with
       "main" -> "return 0;\n"
-    | _      -> "" ) ^ "}"
+    | _      -> "" ) ^ "}\n"
 
 let generate funcs name =
   let outfile = open_out ("tests/" ^ name ^ "-NEW.c")
-  in let translated_program =  "#include <stdio.h>\n" ^ String.concat "" (List.map gen_fdecl funcs) ^ "\n"
+  in let translated_program =  "#include <stdio.h>\n" ^ String.concat "" (List.rev (List.map gen_fdecl funcs)) ^ "\n"
   in ignore(Printf.fprintf outfile "%s" translated_program);
   close_out outfile;
--- frac.ml
+++ frac.ml
@@ -8,17 +8,8 @@ let get_prog_name source_file_path =
 		List.nth split_name ((List.length split_name) - 2)
 
 let _ =
-(*   let action = if Array.length Sys.argv > 1 then
-    List.assoc Sys.argv.(1) [ ("-a", Ast);
-            ("-c", Compile) ]
-  else Compile in *)
   let name = get_prog_name Sys.argv.(1) in
   let input = open_in Sys.argv.(1) in
   let lexbuf = Lexing.from_channel input in
   let program = Parser.program Scanner.token lexbuf in
   Compile.generate program name
-
-(*   match action with
-    Ast -> let listing = Ast.string_of_program program
-           in print_string listing
-  | Compile -> Compile.translate program *)
