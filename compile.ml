8ed2368c64ab90316e839e6fe6fe1de60fbc5bd3
Annie Zhang <azhang496@gmail.com>
2015-11-17 00:01:39 -0500

    Compiles most expressions and statements

--- compile.ml
+++ compile.ml
@@ -14,21 +14,31 @@ let rec expr = function
     Literal i -> string_of_int i
   | Id s -> s
   | String s -> "\"" ^ s ^ "\""
+  | Binop (e1, o, e2) -> (expr e1) ^ (
+      match o with
+        Add -> " + " | Sub -> " - " | Mult -> " * " | Div -> " / "
+      | Equal -> " == " | Neq -> " != "
+      | Less -> " < " | Leq -> " <= " | Greater -> " > " | Geq -> " >= "
+    ) ^ (expr e2)
+  | Assign (v, e) -> v ^ " = " ^ (expr e)
   | Call (fname, actuals) -> (match fname with
     "print" -> "printf(\"%s\", " ^ (expr (List.hd actuals)) ^ ")"
-(*         "print" -> "printf(" ^ expr actuals ^ ")" *)
-(*       | _       -> fname ^ "(" ^ expr actuals ^ ")") *)
     | _     -> "")
   | Noexpr -> ""
 
 let rec stmt = function
     Block sl -> String.concat "" (List.map stmt sl)
-  | Expr e -> expr e ^ ";\n"
+  | Expr e -> (expr e) ^ ";\n"
+  | Return e -> "return " ^ (expr e) ^ ";\n"
+  | If (e, st, Block[]) -> "if(" ^ (expr e) ^ ") {\n" ^ (stmt st) ^ "}\n"
+  | If (e, st1, st2) -> "if(" ^ (expr e) ^ ") {\n" ^ (stmt st1) ^ "}\n" ^
+      "else" ^ "{\n" ^ (stmt st2) ^ "}\n"
+  | While (e, st) -> "while(" ^ (expr e) ^ ") {\n" ^ (stmt st) ^ "}\n"
 
 let gen_fdecl fdecl =
   (match fdecl.fname with
       "main" -> "int main()"
-    | _      -> "func " ^ fdecl.fname) ^
+    | _      -> "func " ^ fdecl.fname) ^ (* THIS IS WRONG *)
 "{\n" ^ String.concat "" (List.map stmt fdecl.body) ^
   (match fdecl.fname with
       "main" -> "return 0;\n"
