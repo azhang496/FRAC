766e4e781f078feeb3392a052ce8279e65189ee1
Kunal Kamath <kkamath101@gmail.com>
2015-12-21 06:58:32 -0500

    Made sutre test suite works for demo

--- tests/pass/test-gram_grow.c
+++ tests/pass/test-gram_grow.c
@@ -2,53 +2,55 @@
 #include <string.h>
 #include <stdio.h>
 
-void koch(char var, int iter) {
-if (iter < 0) {
-if (var == 'F') {
-turtle_forward(1);
-}
-} else {
-if(var == 'F') {
-koch('F', iter - 1);
-koch('m', iter - 1);
-koch('F', iter - 1);
-koch('p', iter - 1);
-koch('p', iter - 1);
-koch('F', iter - 1);
-koch('m', iter - 1);
-koch('F', iter - 1);
-}
-if (var == 'm') {
-turtle_turn_right(60);
-}
-if (var == 'p') {
-turtle_turn_left(60);
-}
-}
-}
-void koch_start(int iter) {
-koch('F', iter);
-koch('p', iter);
-koch('p', iter);
-koch('F', iter);
-koch('p', iter);
-koch('p', iter);
-koch('F', iter);
-}
-int main(){
-turtle_init(2000, 2000);
-koch_start(5);
-turtle_save_bmp("koch.bmp");
-turtle_cleanup();
-char buf[1024];
-int i;
-for(i = 0; i <5; i++) {
-turtle_init(2000, 2000);
-koch_start(i+1);
-sprintf(buf, "koch%d.bmp", i);
-turtle_save_bmp(buf);
-turtle_cleanup();
-}
-return 0;
+void koch(char var, int iter, int step) {
+  if (iter < 0) {
+    if (var == 'F') {
+      turtle_forward(step);
+    }
+  } else {
+    if(var == 'F') {
+      koch('F', iter - 1, step);
+      koch('m', iter - 1, step);
+      koch('F', iter - 1, step);
+      koch('p', iter - 1, step);
+      koch('p', iter - 1, step);
+      koch('F', iter - 1, step);
+      koch('m', iter - 1, step);
+      koch('F', iter - 1, step);
+    }
+    if (var == 'm') {
+      turtle_turn_right(60);
+    }
+    if (var == 'p') {
+      turtle_turn_left(60);
+    }
+  }
 }
 
+void koch_start(int iter, int step) {
+  koch('F', iter, step);
+  koch('p', iter, step);
+  koch('p', iter, step);
+  koch('F', iter, step);
+  koch('p', iter, step);
+  koch('p', iter, step);
+  koch('F', iter, step);
+}
+
+int main() {
+  turtle_init(2000, 2000);
+  koch_start(5, 1);
+  turtle_save_bmp("koch.bmp");
+  turtle_cleanup();
+  char buf[1024];
+  int i;
+  int arr[] = {50,20,8,3,1};
+  for(i = 0; i <5; i++) {
+    turtle_init(2000, 2000);
+    koch_start(i+1, arr[i]);
+    sprintf(buf, "koch%02d.bmp", i);
+    turtle_save_bmp(buf);
+    turtle_cleanup();
+  }
+  return 0;
+}
--- tests/pass/test-koch_gram.c
+++ tests/pass/test-koch_gram.c
@@ -2,45 +2,45 @@
 #include <string.h>
 #include <stdio.h>
 
-void koch_flake(char var, int iter) {
-    if (iter < 0) {
-        if (var == 'F') {
-            turtle_forward(1);
-        }
-    } else {
-        if (var == 'F') {
-            koch_flake('F', iter - 1);
-            koch_flake('m', iter - 1);
-            koch_flake('F', iter - 1);
-            koch_flake('p', iter - 1);
-            koch_flake('p', iter - 1);
-            koch_flake('F', iter - 1);
-            koch_flake('m', iter - 1);
-            koch_flake('F', iter - 1);
-        }
-        if (var == 'p') {
-            turtle_turn_left(60);
-        }
-        if (var == 'm') {
-            turtle_turn_right(60);
-        }
+void koch(char var, int iter, int step) {
+  if (iter < 0) {
+    if (var == 'F') {
+      turtle_forward(step);
     }
+  } else {
+    if(var == 'F') {
+      koch('F', iter - 1, step);
+      koch('m', iter - 1, step);
+      koch('F', iter - 1, step);
+      koch('p', iter - 1, step);
+      koch('p', iter - 1, step);
+      koch('F', iter - 1, step);
+      koch('m', iter - 1, step);
+      koch('F', iter - 1, step);
+    }
+    if (var == 'p') {
+      turtle_turn_left(60);
+    }
+    if (var == 'm') {
+      turtle_turn_right(60);
+    }
+  }
 }
 
-void koch_flake_start(int iter) {
-    koch_flake('F', iter);
-    koch_flake('p', iter);
-    koch_flake('p', iter);
-    koch_flake('F', iter);
-    koch_flake('p', iter);
-    koch_flake('p', iter);
-    koch_flake('F', iter);
+void koch_start(int iter, int step) {
+  koch('F', iter, step);
+  koch('p', iter, step);
+  koch('p', iter, step);
+  koch('F', iter, step);
+  koch('p', iter, step);
+  koch('p', iter, step);
+  koch('F', iter, step);
 }
 
 int main() {
-    turtle_init(2000,2000);
-    koch_flake_start(5);
-    turtle_save_bmp("koch_flake.bmp");
+    turtle_init(2000, 2000);
+    koch_start(5, 1);
+    turtle_save_bmp("koch.bmp");
     turtle_cleanup();
     return 0;
 }
