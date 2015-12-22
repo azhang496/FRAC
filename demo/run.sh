0e14ecfe3efbe1fc4c135b0ca833915bd5c0f963
Kunal Kamath <kkamath101@gmail.com>
2015-12-22 15:15:51 -0500

    Scripts in place

--- demo/run.sh
+++ demo/run.sh
@@ -2,19 +2,21 @@
 shopt -s nullglob
 arg=$1
 exe=${arg/.frac/}
+
+# Compile frac program into c
 .././frac $arg
+
+# Make the c file into executable
 make
 ./$exe
+
+# If the output is a GIF
 files=( *[0-9]-GIF.bmp )
 num=${#files[@]}
-
-# NEED TO CHECK IF CORRECT PACKAGES ARE INSTALLED OR NOT
-# IF NOT, JUST LEAVE THEM WITH THE 5 BMP FILES
 if [ $num -gt 2 ]; then
   gm convert -adjoin ${files[@]} tmp.gif || { echo 'you must run ./configure in top-level directory before generating GIFs' ; exit 1; }
   gifsicle --delay=50 --loop tmp.gif > $exe.gif || { echo 'you must run ./configure in top-level directory before generating GIFs' ; exit 1; }
   rm tmp.gif ${files[@]}
-  open $exe.gif
 fi
-open *.bmp
+
 exit 0
--- run.sh
+++ run.sh
@@ -2,19 +2,22 @@
 shopt -s nullglob
 arg=$1
 exe=${arg/.frac/}
+
+# Compile frac program into c
 ./frac $arg
-gcc -g -Wall   -c -o $exe.o $exe.c
-gcc -g -Wall   -c -o turtle.o turtle.c
-gcc -g -lm  $exe.o turtle.o   -o $exe
+
+# Make the c file into executable
+gcc -g -Wall -Ilib/ -c -o $exe.o $exe.c
+gcc -g -Wall -c -o lib/turtle.o lib/turtle.c
+gcc -g -lm lib/turtle.o $exe.o -o $exe || { echo 'C' ; exit 1; }
 ./$exe
+
+# If the output is a GIF
 files=( *[0-9]-GIF.bmp )
 num=${#files[@]}
-
 if [ $num -gt 1 ]; then
   gm convert -adjoin ${files[@]} tmp.gif || { echo 'you must run ./configure in top-level directory before generating GIFs' ; exit 1; }
   gifsicle --delay=50 --loop tmp.gif > $exe.gif || { echo 'you must run ./configure in top-level directory before generating GIFs' ; exit 1; }
   rm tmp.gif ${files[@]}
-  open $arg.gif
 fi
-open *.bmp
 exit 0
